821ac5131fed0771eecad7aceb8ee8b84dd54ba5
0Much0 <2085149940@qq.com>
2024-10-14 22:56:41 +0800

    Add files via upload

--- test_10_13/test2/test2.c
+++ test_10_13/test2/test2.c
@@ -3,9 +3,14 @@ int main()
 {
     int now = 0; //初始化
     int pass = 0;
+    printf("请以'四位整数+任意整数'的形式输入，表示起始的时间+流逝的分钟数:");
     scanf("%d %d", &now,&pass); //输入
     int hour = (now/100 + pass/60) + (now%100 + pass%60)/60;
     int min = (now%100 + pass%60)%60;
+    if (min<0){
+        min = 60 + min;
+        hour--;
+    }
     int final = (hour*100 + min);
     printf("%d",final); //输出
 
