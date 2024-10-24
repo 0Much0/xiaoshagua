56dc1b94315ac445a49095c247752917c4bd2a86
0Much0 <2085149940@qq.com>
2024-10-24 17:22:07 +0800

    Add files via upload

--- test_10_23/test2/test2.c
+++ test_10_23/test2/test2.c
@@ -1,9 +1,21 @@
+// 计算int类型的最大数可以是多少
 #include<stdio.h>
 
 int main()
 {
-    int a = sizeof(int);
-    printf("%d",a);
+    int a = 0,b = 0;
+    // 第一个while循环：找到int类型的最大值
+    while (++a>0);
+    // 结束循环时a会超出正数范围，值会变为负数
+    printf("int类型的最大数为:%d\n",a-1);// a - 1 即为 int 类型的最大值
+    b++;// 数位计数器，至少有一位
+    while (a=a/10)// 通过连续除以10，计算最大值有多少位
+    {
+        printf("%d");
+        b++;
+    }
+    
+    printf("int类型的最大数的数位为:%d位",b);
 
     return 0;
 }
